fc103f3af011c512ef937e5a12f4163e6318b40c
dipta ikromi m <dipta.ikromi@unej.ac.id>
2024-02-21 03:15:42 +0700

    mengembangkan auth page

--- src/main.js
+++ src/main.js
@@ -17,6 +17,9 @@ import {
 } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+defineCustomElements(window);
+
 
 const vuetify = createVuetify({
     components,
